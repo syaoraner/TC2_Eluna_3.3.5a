dfbef02d9df558c25729265ced11f96310f07d0b
SymbolixDEV <shabi_bjk@abv.bg>
2013-12-08 11:07:28 +0200

    Added VIP SQL

---
+++ sql/VIP.sql
@@ -0,0 +1,5 @@
+INSERT INTO `command` VALUES ('vip mall', '1', '');
+INSERT INTO `command` VALUES ('vip changerace', '1', '');
+INSERT INTO `command` VALUES ('vip changefaction', '1', '');
+INSERT INTO `command` VALUES ('vip customize', '1', '');
+INSERT INTO `command` VALUES ('vip tele', '1', '');
